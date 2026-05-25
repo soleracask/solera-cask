4a92a8ccaccd0e64e71b6e53d0632e6cebc37e62
imaj0711 <i.m.aldomar@gmail.com>
2026-05-25 13:25:30 +0200

    post page breadcrumb styling updates

--- netlify/functions/post-page.js
+++ netlify/functions/post-page.js
@@ -203,7 +203,7 @@ function generatePostHTML(post) {
     <style>
         /* Post page specific styles */
         .post-page {
-            padding-top: 120px;
+            padding-top: 80px;
             min-height: 100vh;
             background: var(--warm-white);
         }
@@ -380,6 +380,7 @@ function generatePostHTML(post) {
     border-bottom: 1px solid var(--border-light);
     padding: 12px 0;
     width: 100%;
+    margin-bottom: 40px;
 }
 
 .breadcrumb-inner {
